164372e5a9227a15dca161b80b84605099e5e72c
Stanislav Sandalnikov <s.sandalnikov@gmail.com>
2017-04-23 13:52:40 +0700

    Refactor private methods

--- lib/phabricator.rb
+++ lib/phabricator.rb
@@ -12,23 +12,37 @@ class Phabricator
   def user_search(options = {})
     endpoint = 'user.search'
 
-    call(endpoint, options)
+    paginate(endpoint, options)
   end
 
   def maniphest_search(options = {})
     endpoint = 'maniphest.search'
 
-    call(endpoint, options)
+    paginate(endpoint, options)
   end
 
   def maniphest_info(task_id)
     endpoint = 'maniphest.info'
-    url = "#{@url}#{endpoint}"
+    payload = { task_id: task_id }
 
-    JSON.parse(RestClient.post(url, { task_id: task_id, 'api.token': @api_token}).body)['result']
+    call(endpoint, payload)
+  end
+
+  def maniphest_gettasktransactions(task_id)
+    endpoint = 'maniphest.gettasktransactions'
+    payload = { task_id: task_id }
+
+    call(endpoint, payload)[task_id]
   end
 
   private def call(endpoint, payload)
+    url = "#{@url}#{endpoint}"
+    payload = { 'api.token' => @api_token }.merge(payload)
+
+    JSON.parse(RestClient.post(url, payload).body)['result']
+  end
+
+  private def paginate(endpoint, payload)
     url = "#{@url}#{endpoint}"
     payload = { 'api.token' => @api_token }.merge(payload)
     data = []
