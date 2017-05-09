6f04a6af02b7d254b42e11583d7c8d8246f9292c
Stanislav Sandalnikov <s.sandalnikov@gmail.com>
2017-05-10 00:02:35 +0700

    Add convertion for unordered lists starting with '#'

--- lib/phabricator.rb
+++ lib/phabricator.rb
@@ -8,7 +8,7 @@ class Phabricator
   end
 
   def self.format_message(msg)
-    msg = msg.gsub(/^\[\]/, '* [ ]').gsub(/^\[x\]/, '* [x]').gsub(/^lang=.*$/, '')
+    msg = msg.gsub(/^\[\]/, '* [ ]').gsub(/^\[x\]/, '* [x]').gsub(/^lang=.*$/, '').gsub(/^\s*#/, '*')
 
     return msg
   end
